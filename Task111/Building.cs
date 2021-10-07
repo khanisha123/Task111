52bbc6ce2c0a25a0b81c6534d1d6714beed6f7c8
khanisha123 <khanisha@code.edu.az>
2021-10-07 05:15:34 +0400

    Add project files.

---
+++ Task111/Building.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task111
+{
+    class Building
+    {
+        public string Model;
+        public int Height;
+        public int Width;
+        public int Length;
+        public int NumberOfWindows;
+        public int HowManySquareMeteers;
+        public int NumberOfDoor;
+        public int HowManyFloors;
+        public int Price;
+
+    }
+    //public static void Detail()
+    //{
+    //    return $"{ Model }" +
+    //        $"{Height}" +
+    //        $"{Width} " +
+    //        $"{Length} " +
+    //        $"{NumberOfWindows} " +
+    //        $"{HowManySquareMeteers} " +
+    //        $"{NumberOfDoor} " +
+    //        $"{HowManyFloors}" +
+    //        $" {Price} ";
+
+    //}
+}
